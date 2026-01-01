175a8fa7fb8922ce7ef3198d6b8269acb92cfdf9
Usama Khalid <usamak5453@gmail.com>
2026-01-01 22:34:10 +0500

    Add Day 001 - Band Name Generator

---
+++ Day-001-band-name-generator/band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator")
+city_name = input("Enter the name of city where you born: ").capitalize()
+pet_name = input("What your Pet's Name: ").capitalize()
+print(f"Your band name could be {city_name} {pet_name}")
