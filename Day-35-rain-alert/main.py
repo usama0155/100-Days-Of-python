4d7b214c8c2f0e54624ead42c09e5c74d87eafec
Usama Khalid <usamak5453@gmail.com>
2026-01-21 20:40:05 +0500

    Added Day 35 - Rain Alert

---
+++ Day-35-rain-alert/main.py
@@ -0,0 +1,38 @@
+import os
+import requests
+
+
+endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = os.environ.get("owm_api_key")
+TOPIC = os.environ.get("topic") 
+url = f"https://ntfy.sh/{TOPIC}"
+
+
+parameter = {
+    "q" : "shaoyang",
+    "appid" : api_key,
+    "cnt" : 4
+}
+
+response = requests.get(endpoint, params=parameter)
+response.raise_for_status()
+weather_data = response.json()
+
+is_it_rainig = False
+for hourly_data in weather_data['list']:
+    weather_code = hourly_data['weather'][0]['id']
+    if int(weather_code) < 600:
+        is_it_rainig = True
+
+
+def send_notification(message, title="Notification"):
+    """Sends a notification"""
+    requests.post(
+        url,
+        headers={"Title": title},
+        data=message.encode('utf-8')
+        )
+    print("✓ Notification sent!")
+
+if is_it_rainig:
+    send_notification("It's gonna rain", "Rain Alert")
